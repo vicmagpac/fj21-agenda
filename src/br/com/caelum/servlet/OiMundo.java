3abf611245be05e31587fe03efc87d72d273a26f
Victor Magalhaes Pacheco <victor.magalhaesp@gmail.com>
2015-05-05 12:06:22 -0300

    Adicionando parâmetros ao iniciar a servlet

--- src/br/com/caelum/servlet/OiMundo.java
+++ src/br/com/caelum/servlet/OiMundo.java
@@ -2,14 +2,27 @@ package br.com.caelum.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "MinhaServlet3", urlPatterns = {"/oi", "/ola"})
+@WebServlet(
+		name = "MinhaServlet3", 
+		urlPatterns = {"/oi"},
+		initParams = {
+				@WebInitParam(name = "param1", value = "value"),
+				@WebInitParam(name = "param1", value = "value2")
+				
+		}
+)
 public class OiMundo extends HttpServlet {
+	
+	private String parametro1;
+	private String parametro2;
 	
 	@Override
 	protected void service (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
@@ -18,9 +31,16 @@ public class OiMundo extends HttpServlet {
 		// escreve o texto
 		out.println("<html>");
 		out.println("<body>");
-		out.println("Primeira Servlet");
+		out.println("Primeira Servlet: " + this.parametro1);
 		out.println("</body>");
 		out.println("</html");
 		
+	}
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		this.parametro1 = config.getInitParameter("param1");
+		this.parametro2 = config.getInitParameter("param2");
 	}
 }
