d1ffb71c908000b82424d095a013a7a93ae78a38
Victor Magalhaes Pacheco <victor.magalhaesp@gmail.com>
2015-05-04 22:35:51 -0300

    Usando a anotação do WebServlet 3.0

--- src/br/com/caelum/servlet/OiMundo.java
+++ src/br/com/caelum/servlet/OiMundo.java
@@ -3,11 +3,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/oi")
 public class OiMundo extends HttpServlet {
 	
 	@Override
