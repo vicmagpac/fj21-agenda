4338da07a042c302f2d6b7a2debdab93b27e7751
Victor Magalhaes Pacheco <victor.magalhaesp@gmail.com>
2015-05-05 23:26:29 -0300

    Mostrando que cada requisição se cria a mesma thread

---
+++ src/br/com/caelum/servlet/Contador.java
@@ -0,0 +1,28 @@
+package br.com.caelum.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/contador")
+public class Contador extends HttpServlet {
+	private int contador = 0;
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		contador++;
+		
+		PrintWriter out = response.getWriter();
+		
+		// escreve o texto
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Contador agora é: " + contador);
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
